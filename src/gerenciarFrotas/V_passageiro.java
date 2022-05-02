fd84842366ba974a42ff2933ae44f97f9417d4fe
gtdfgKekw <102192917+gtdfgKekw@users.noreply.github.com>
2022-05-02 12:46:42 -0300

    Update V_passageiro.java

--- src/gerenciarFrotas/V_passageiro.java
+++ src/gerenciarFrotas/V_passageiro.java
@@ -8,6 +8,20 @@ public class V_passageiro extends V_utilitario{
 	int maxOcupantes;
 	boolean arCondicionado, direcaoHidraulica, tv;
 	
+	public V_passageiro() {
+		
+	}
+	
+	public V_passageiro(String marca, String modelo, String renavam, int anoFabricacao, int anoModelo,
+			int capacidadeTanque) {
+		setMarca(marca);
+		setModelo(modelo);
+		setRenavam(renavam);
+        setAnoFabricacao(anoFabricacao);
+        setAnoModelo(anoModelo);
+        setCapacidadeTanque(capacidadeTanque);
+	}
+
 	public int getMaxOcupantes(){
         
 
